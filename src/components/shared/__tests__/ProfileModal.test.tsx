5bd21e63b385f1cc7e0f265b1c681112af2e8c97
JungHoon Park <wfr112@naver.com>
2020-03-25 22:43:06 +0900

    Update <ProfileModal/> test case

--- src/components/shared/__tests__/ProfileModal.test.tsx
+++ src/components/shared/__tests__/ProfileModal.test.tsx
@@ -1,66 +1,68 @@
-import ProfileModal, { Ref } from '../ProfileModal';
-import React, { ReactElement, createRef } from 'react';
-import { fireEvent, render } from '@testing-library/react-native';
-import { MUTATION_DELETE_FRIEND } from '../../../graphql/mutations';
-import { MockedProvider } from '@apollo/react-testing';
+import { MockedProvider, MockedResponse } from '@apollo/react-testing';
+import React, { ReactElement } from 'react';
+import Friend from '../../screen/Friend';
+import { QUERY_FRIENDS } from '../../../graphql/queries';
 import { createTestElement } from '../../../../test/testUtils';
+import { render } from '@testing-library/react-native';
 
 describe('[ProfileModal] rendering test', () => {
-  const ref = createRef<Ref>();
-  const setup = (mocks?): ReactElement =>
-    createTestElement(
+  const setup = (mocks?: Array<MockedResponse>): ReactElement => {
+    // Insert mock at index of 0
+    mocks && mocks.unshift({
+      request: {
+        query: QUERY_FRIENDS,
+      },
+      result: {
+        data: {
+          friends: [{
+            id: 'aa11',
+            photoURL: '',
+            name: 'testName',
+          }],
+        },
+      },
+    });
+
+    return createTestElement(
       <MockedProvider mocks={mocks} addTypename={false}>
-        <ProfileModal ref={ref} />
+        <Friend />
       </MockedProvider>);
+  };
 
   it('Render without crashing', () => {
     const component = render(setup());
     expect(component).toMatchSnapshot();
   });
 
-  it('Ref must not be null', () => {
-    expect(ref.current).not.toBeNull();
-  });
-
-  it('Open', () => {
-    const component = render(setup());
-    ref.current.open();
-    expect(component.getByTestId('btn-ad-friend')).not.toBeNull();
-  });
-
-  it('Close', () => {
-    const component = render(setup());
-    ref.current.close();
-    // Expects dom is null
-    expect(component.queryByTestId('btn-ad-friend')).toBeNull();
-  });
-
+  /*
   it('Delete existing user', () => {
-    // TODO: 1. Add a friend
-    // 2. Delete a friend
     const mocks = [
       {
         request: {
           query: MUTATION_DELETE_FRIEND,
           variables: {
-            id: '8b3ee240-5545-11ea-9ea9-ad4e7fcc8ca2',
+            id: 'aa11',
           },
         },
         result: {
           data: {
-            id: '8b3ee240-5545-11ea-9ea9-ad4e7fcc8ca2',
+            id: 'aa11',
           },
         },
       },
     ];
 
     const component = render(setup(mocks));
-    ref.current.open();
-    const button = component.getByTestId('btn-ad-friend');
-    fireEvent.press(button);
+    const friend = component.getByTestId('user-id-0');
+    fireEvent.press(friend);
 
-    // TODO: 3. Check whether it deleted from View.
+    // TODO: find profileModal's delete button and press it.
+    // const profileModalButton = profileModal.getByTestId('btn-ad-friend');
+    // fireEvent.press(friendModalButton);
+
+    // TODO: Check whether it deleted from View.
   });
+  */
 
   // it("Delete not existing user", () => {})
   // it("Delete a friend and check error state")
