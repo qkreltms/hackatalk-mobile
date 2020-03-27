4460d4ffa439ddb9c7882ce836d5c0ba0ca3d351
qkreltms <junghooncentralpark@gmail.com>
2020-03-27 20:58:32 +0900

    Delete comments

--- src/components/shared/__tests__/ProfileModal.test.tsx
+++ src/components/shared/__tests__/ProfileModal.test.tsx
@@ -1,9 +1,9 @@
 import { MockedProvider, MockedResponse } from '@apollo/react-testing';
 import React, { ReactElement } from 'react';
+import { render, wait } from '@testing-library/react-native';
 import Friend from '../../screen/Friend';
 import { QUERY_FRIENDS } from '../../../graphql/queries';
 import { createTestElement } from '../../../../test/testUtils';
-import { render } from '@testing-library/react-native';
 
 describe('[ProfileModal] rendering test', () => {
   const setup = (mocks?: Array<MockedResponse>): ReactElement => {
@@ -29,41 +29,10 @@ describe('[ProfileModal] rendering test', () => {
       </MockedProvider>);
   };
 
-  it('Render without crashing', () => {
-    const component = render(setup());
-    expect(component).toMatchSnapshot();
+  it('Render without crashing', async () => {
+    await wait(() => {
+      const component = render(setup());
+      expect(component).toMatchSnapshot();
+    });
   });
-
-  /*
-  it('Delete existing user', () => {
-    const mocks = [
-      {
-        request: {
-          query: MUTATION_DELETE_FRIEND,
-          variables: {
-            id: 'aa11',
-          },
-        },
-        result: {
-          data: {
-            id: 'aa11',
-          },
-        },
-      },
-    ];
-
-    const component = render(setup(mocks));
-    const friend = component.getByTestId('user-id-0');
-    fireEvent.press(friend);
-
-    // TODO: find profileModal's delete button and press it.
-    // const profileModalButton = profileModal.getByTestId('btn-ad-friend');
-    // fireEvent.press(friendModalButton);
-
-    // TODO: Check whether it deleted from View.
-  });
-  */
-
-  // it("Delete not existing user", () => {})
-  // it("Delete a friend and check error state")
 });
